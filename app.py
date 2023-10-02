4455cb4dd3b716c556cacfbfb10b625e6932ba90
Subhankar Panda <91069648+Wanderer-of-the-abyss@users.noreply.github.com>
2023-10-02 23:08:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,21 @@
-
 import streamlit as st
 from streamlit_cookies_manager import EncryptedCookieManager
 import os
 import warnings
 
 cookies = EncryptedCookieManager(
-    # This prefix will get added to all your cookie names.
-    # This way you can run your app on Streamlit Cloud without cookie name clashes with other apps.
     prefix="LUL/streamlit-cookies-manager/",
-    # You should really setup a long COOKIES_PASSWORD secret if you're running on Streamlit Cloud.
     password=os.environ.get("COOKIES_PASSWORD", "uDnda87,kGFdi&jh.kjsk/jk4DF369*^jhGks"),
 )
 warnings.filterwarnings("ignore")
 import uuid
 from datetime import datetime, timedelta
 
-@st.cache_data
-def gener():
-   user_id = str(uuid.uuid4()) # generate a random user id
+user_id = cookies.get('user_id')  # Attempt to retrieve the user ID cookie
 
-   cookies['user_id'] = user_id# set the cookie
-
-user_id = cookies['user_id'] # get the cookie value
 if user_id is not None:
-    st.write(f"Your user id is {user_id}") # display the user id
+    st.write(f"Your user id is {user_id}")  # Display the user ID
 else:
-    gener()
+    user_id = str(uuid.uuid4())  # Generate a random user ID
+    cookies['user_id'] = user_id  # Set the cookie
+    st.write(f"Welcome! Your user id is {user_id}")  # Display the newly generated user ID
